132f51ee70625e54ece1e2dc044aa01e62f12441
Rhysbormolini <61007759+Rhysbormolini@users.noreply.github.com>
2021-02-10 23:36:34 +0800

    Implemented different scenes, movement, flipping of character

---
+++ 1 Week Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float walkSpeed;
+    private bool facingRight = true;
+
+    private void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        if (move < 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(move * walkSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (move > 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(move * walkSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (facingRight == false && move > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && move < 0)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
---
+++ 1 Week Game/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+
+    public GameObject goodButton, badButton, canvas;
+    public string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canvas.SetActive(true);
+        }
+    }
+
+    public void goodOption()
+    {
+        if (sceneName == "Starting Scene")
+        {
+            SceneManager.LoadScene(2);
+        }
+
+        if (sceneName == "1Good")
+        {
+            SceneManager.LoadScene(4);
+        }
+
+        if (sceneName == "2Good")
+        {
+            SceneManager.LoadScene(5);
+        }
+
+        if (sceneName == "1Bad")
+        {
+            SceneManager.LoadScene(4);
+        }
+
+        if (sceneName == "2Bad")
+        {
+            SceneManager.LoadScene(5);
+        }
+    }
+
+    public void badOption()
+    {
+        if (sceneName == "Starting Scene")
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if (sceneName == "1Good")
+        {
+            SceneManager.LoadScene(3);
+        }
+
+        if (sceneName == "2Good")
+        {
+            SceneManager.LoadScene(5);
+        }
+
+        if (sceneName == "1Bad")
+        {
+            SceneManager.LoadScene(3);
+        }
+
+        if (sceneName == "2Bad")
+        {
+            SceneManager.LoadScene(5);
+        }
+    }
+}
